531d2ade7e327002c13c157c37a28b6a8b8e732e
GuilhermeCF <simulator@agorasimlab.com>
2025-12-14 22:31:08 +0100

    feat: Add simulation loading functionality and time conversion utilities

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from src import Simulation, show_style
 # Inspect the Portugal style
 #show_style(style_label="portugal_style")
 
-sim = Simulation.random(10)
+#sim = Simulation.random(10)
+#sim.run()
 
-sim.run()
+sim = Simulation.load("runs/simulation_run_20251214_221359.pkl")
+
+sim.summary()
--- src/engine/simulation.py
+++ src/engine/simulation.py
@@ -1,6 +1,8 @@
 # ================================================================
 # 0. Section: Imports
 # ================================================================
+import pickle
+
 import numpy as np
 from copy import deepcopy
 
@@ -8,6 +10,7 @@ from ..logger import logger
 from ..components import Citizen
 from .utils import initiate_connectivity_matrix
 from .engine_pieces import Properties, Run
+from ..time import iterations_to_simtime
 
 
 class Simulation(Properties, Run):
@@ -17,7 +20,7 @@ class Simulation(Properties, Run):
     def __init__(self, citizens: np.ndarray, connectivity_matrix: np.ndarray):
         self.citizens = citizens
         self.connectivity_matrix = connectivity_matrix
-        
+
         self.iterations = 0
 
         self.citizens_history = [deepcopy(self.citizens)]
@@ -33,3 +36,25 @@ class Simulation(Properties, Run):
         logger.debug(f"Connectivity Matrix:\n{connectivity}")
 
         return cls(citizens, connectivity)
+    
+    @classmethod
+    def load(cls, filepath: str) -> "Simulation":
+        with open(filepath, 'rb') as f:
+            citizens_history, connectivity_history = pickle.load(f)
+
+        last_citizens = citizens_history[-1]
+        last_connectivity = connectivity_history[-1]
+
+        logger.info(f"Loaded simulation from {filepath}")
+        logger.info(f"Simulation had {len(last_citizens)} citizens")
+        logger.info(f"Simulation lasted for {len(citizens_history) - 1} iterations")
+        logger.info(f"Simulation lasted {iterations_to_simtime(len(citizens_history) - 1).years} years and {iterations_to_simtime(len(citizens_history) - 1).months} months")
+        logger.debug(f"Citizens:\n{last_citizens}")
+        logger.debug(f"Connectivity Matrix:\n{last_connectivity}")
+
+        recover_simulation = cls(last_citizens, last_connectivity)
+        recover_simulation.citizens_history = citizens_history
+        recover_simulation.connectivity_matrix_history = connectivity_history
+        recover_simulation.iterations = len(citizens_history) - 1
+
+        return recover_simulation
