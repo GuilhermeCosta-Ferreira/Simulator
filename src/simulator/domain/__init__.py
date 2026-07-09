d6b64c421c2e6231067f9874d049a8d9fc8e8f19
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-07-09 10:49:31 +0100

    refactor: fixed extensive imports and pyright flags in loader

---
+++ src/simulator/domain/__init__.py
@@ -0,0 +1,11 @@
+from .node import Node
+from .connectivity_matrix import ConnectivityMatrix
+from .simulation_state import SimulationState
+from .simulation_engine import SimulationEngine
+
+__all__ = [
+    "Node",
+    "ConnectivityMatrix",
+    "SimulationState",
+    "SimulationEngine",
+]
---
+++ src/simulator/domain/instantiation/__init__.py
@@ -0,0 +1,13 @@
+from .step_type import StepType
+from .node_property import NodeProperty
+from .node_blueprint import NodeBlueprint
+from .simulation_specs import SimulationSpecs
+from .simulation_blueprint import SimulationBlueprint
+
+__all__ = [
+    "StepType",
+    "NodeProperty",
+    "NodeBlueprint",
+    "SimulationSpecs",
+    "SimulationBlueprint",
+]
