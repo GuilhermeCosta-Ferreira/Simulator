a5a5527ee16b8897afc8fb7c840b64a7c475dd9f
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-07-16 09:59:52 +0100

    feat: finished implementing figure exporter

--- src/simulator/adapters/render/figure_exporter.py
+++ src/simulator/adapters/render/figure_exporter.py
@@ -17,4 +17,8 @@ class FigureExporter:
     source: Source
 
     def export(self, figure: Figure, name: str, formats: list[str]) -> list[Path]:
-        pass
+        figures_path = self.source.figures_folder
+
+        for fmt in formats:
+            figure.savefig(figures_path / f"{name}.{fmt}", format=fmt)
+        return [figures_path / f"{name}.{fmt}" for fmt in formats]
